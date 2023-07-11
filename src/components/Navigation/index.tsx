e85983e3c70dcf390848fd29d6c41c9ba238c1e4
Yusuf Yalim <yusufyalim@gmail.com>
2023-07-11 13:42:10 +0200

    Fix social links taking all space

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,8 +1,12 @@
 export default function Navigation() {
   return (
-    <nav className="navigation">
-      <a target="_blank" href="https://www.linkedin.com/in/yusufyalim/">LinkedIn</a>
-      <a target="_blank" href="https://www.github.com/yyalim">Github</a>
-    </nav>
+    <ul className="social-links">
+      <li>
+        <a target="_blank" href="https://www.linkedin.com/in/yusufyalim/">LinkedIn</a>
+      </li>
+      <li>
+        <a target="_blank" href="https://www.github.com/yyalim">Github</a>
+      </li>
+    </ul>
   );
 }
