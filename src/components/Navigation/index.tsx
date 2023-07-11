c4cba0c73c22e690c4a38d73512cb24a2ee94bbf
Yusuf Yalim <yusufyalim@gmail.com>
2023-07-11 14:53:34 +0200

    Use icons for social links

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -2,10 +2,10 @@ export default function Navigation() {
   return (
     <ul className="social-links">
       <li>
-        <a target="_blank" href="https://www.linkedin.com/in/yusufyalim/">LinkedIn</a>
+        <a className="github" target="_blank" href="https://www.linkedin.com/in/yusufyalim/">LinkedIn</a>
       </li>
       <li>
-        <a target="_blank" href="https://www.github.com/yyalim">Github</a>
+        <a className="linkedin" target="_blank" href="https://www.github.com/yyalim">Github</a>
       </li>
     </ul>
   );
