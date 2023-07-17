6f5c3e74d8816a051adff1885ca27ef8958e93f0
Yusuf Yalim <yusufyalim@gmail.com>
2023-07-17 11:41:28 +0200

    Add meta description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,7 @@ export default class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+          <meta name="description" content="Yusuf Yalim's personal website, a software engineer." />
         </Head>
         <body className="dark">
           <Main />
